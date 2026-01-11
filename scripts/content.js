105a35ff433bac4cf1ce8571033b6fc1a570463b
dev <@users.noreply.github.com>
2026-01-11 20:18:14 +0000

    setting up manifest to work on youtube

---
+++ scripts/content.js
@@ -0,0 +1,6 @@
+let thumbnailcollect = document.getElementById("ytThumbnailViewModelImage");
+
+let randomElement = thumbnailcollect[7];
+
+randomElement.classList.add("yt-seen-by-extension");
+console.log(randomElement);
